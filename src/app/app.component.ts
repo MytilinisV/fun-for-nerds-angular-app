2c62f4eb0b9c485ec93ba67127f1d04e0f8a73e7
MytilinisV <vasilismitilinis@gmail.com>
2024-05-01 16:32:51 +0300

    fixing packagejson for bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { FunForNerdsComponent } from "./fun-for-nerds/fun-for-nerds.component";
-import { EvenLengthWordsComponent } from './even-length-words/even-length-words.component';
-import { OddLengthWordsComponentComponent } from './odd-length-words-component/odd-length-words-component.component';
+import { FunForNerdsComponent } from "./components/fun-for-nerds/fun-for-nerds.component";
+import { EvenLengthWordsComponent } from './components/even-length-words/even-length-words.component';
+import { OddLengthWordsComponentComponent } from './components/odd-length-words-component/odd-length-words-component.component';
 
 @Component({
     selector: 'app-root',
     standalone: true,
+    imports: [RouterOutlet, FunForNerdsComponent, EvenLengthWordsComponent, OddLengthWordsComponentComponent],
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, FunForNerdsComponent, EvenLengthWordsComponent, OddLengthWordsComponentComponent, FunForNerdsComponent],
+
 })
 export class AppComponent {
   title = 'fun-for-nerds-angular-app';
